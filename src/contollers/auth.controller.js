0eeadbe74e760aa1f2b728d13c56ada89f8479c0
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-06-23 17:06:28 -0400

    integracion de controller auth

--- src/contollers/auth.controller.js
+++ src/contollers/auth.controller.js
@@ -1,10 +1,12 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcrypt";
 
 async function registerUser(req, res) {
     try {
         const userName = req.body.name;
         const userEmail = req.body.email;
         const password = req.body.password;
+        const userPassword = bcrypt.hashSync(password, 2);
         if(!userName) {return res.status(400).send({success: false, error: 'Falta campo nombre'});}
         if(!userEmail) {return res.status(400).send({success: false, error: 'Falta campo email'});}
         if(!password) {return res.status(400).send({success: false, error: 'Falta campo contraseña'});}
