4e06d8583aea228c6d1a33c3e6a296e5dee3a349
belfor-acuna <bel4negro@gmail.com>
2023-06-23 17:48:42 -0400

    Fix

--- src/contollers/auth.controller.js
+++ src/contollers/auth.controller.js
@@ -3,7 +3,7 @@ import bcrypt from "bcrypt";
 
 async function registerUser(req, res) {
     try {
-        const userName = req.body.name;
+        const username = req.body.username;
         const userEmail = req.body.email;
         const password = req.body.password;
         const userPassword = bcrypt.hashSync(password, 2);
@@ -11,7 +11,7 @@ async function registerUser(req, res) {
         if(!userEmail) {return res.status(400).send({success: false, error: 'Falta campo email'});}
         if(!password) {return res.status(400).send({success: false, error: 'Falta campo contraseña'});}
       await User.create({
-        name: userName,
+        username: username,
         email: userEmail,
         password: userPassword,
       });
