1c8637142f24d4b24750b88f1758a5fd8250011c
belfor-acuna <bel4negro@gmail.com>
2023-06-23 17:15:32 -0400

    Adjustment

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,7 @@
-import Router from "express";
+import editUser from "../contollers/user.controller.js";
+import {Router} from "express";
 
 const router = Router();
+router.put("/profile/edit/:userId",editUser);
 
 export default router;
