8dd5da1af7d8936117a34f6759bd899fc60bd558
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-07-05 18:41:18 -0400

    agregado rutas de administrador y lista de usuarios en controller user

---
+++ src/routes/admin.routes.js
@@ -0,0 +1,13 @@
+import {getUser, listUsers} from "../contollers/user.controller.js";
+import { authRequired , hasRole } from '../middlewares.js';
+import { listCV, deleteCV } from '../contollers/cv.controller.js';
+import { Router } from "express";
+
+const router = Router();
+
+router.get("/:adminId/users", authRequired, hasRole("Admin"), listUsers);
+router.get("/:adminId/users/:pickedUserId", authRequired, hasRole("Admin"), getUser);
+router.get("/:adminId/users/:pickedUserId/cv", authRequired, hasRole("Admin"), listCV);
+router.delete("/:adminId/:pickedUserId/:cvId", hasRole("Admin"), deleteCV);
+
+export default router;
