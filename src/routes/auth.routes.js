7ef7ca2b8e280da1c85dc08ab23a39fe2010b496
belfor-acuna <bel4negro@gmail.com>
2023-07-05 01:18:42 -0400

    getMe middleware addition

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import {registerUser, loginUser,getMe} from "../contollers/auth.controller.js"
-import { authRequired } from '../middlewares.js';
+import { authRequired, hasRole } from '../middlewares.js';
 
 const router = express.Router();
 
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
-router.get("/me",authRequired,getMe);
+router.get("/me",authRequired,hasRole("User"),getMe);
 
 export default router;
